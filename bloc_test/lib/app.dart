a14907c3781487710dc77d21b6fb7271208629ae
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-10-17 13:57:48 +0200

    add[bloc_test]: settings.dart file with necessary exports

--- bloc_test/lib/app.dart
+++ bloc_test/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_counter/settings/view/settings_page.dart';
+import 'package:flutter_counter/settings/settings.dart';
 
 class App extends MaterialApp {
   const App({super.key}) : super(home: const SettingsPage());
