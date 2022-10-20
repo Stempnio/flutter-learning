68dae92829fe09be964d06092d905a724cf638b7
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-10-20 14:31:09 +0200

    add[twitter_clone]: glassmorphic background in app bar

--- twitter_clone/lib/home/view/home_page.dart
+++ twitter_clone/lib/home/view/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/posts/posts.dart';
 
@@ -7,29 +9,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Icon(
-          Icons.flight_takeoff,
-          color: Colors.blueAccent,
-          size: 45,
-        ),
-        leadingWidth: 100,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Icon(Icons.star_border_purple500_outlined),
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 56),
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: AppBar(
+              backgroundColor: Colors.black.withOpacity(0.2),
+              centerTitle: true,
+              title: Icon(
+                Icons.flight_takeoff,
+                color: Colors.blueAccent,
+                size: 45,
+              ),
+              leadingWidth: 100,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(Icons.star_border_purple500_outlined),
+                ),
+              ],
+              leading: Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CircleAvatar(
+                    child: Icon(Icons.person_outline_outlined),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ],
-        leading: Row(
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            CircleAvatar(
-              child: Icon(Icons.person_outline_outlined),
-            ),
-          ],
         ),
       ),
       body: PostsPage(),
