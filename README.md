f38b5ef236eec2a7b0c76d4581589fb486e4e8c4
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-11-17 16:37:55 +0100

    move: twitter clone project to its own branch

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitter clone
+
+## Screenshots
+
+![alt text](https://github.com/Stempnio/flutter-learning/blob/main/app_screenshots/twitter-clone.png?raw=true)
---
+++ lib/bottom_navigator/page_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/home/home.dart';
+
+List<Widget> pageList = [
+  HomePage(),
+  Center(child: Text("2")),
+  Center(child: Text("3")),
+  Center(child: Text("4")),
+];
